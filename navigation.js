87c16c112bd4776a7864d21092e543dd2af33293
Daniela Cuevas <danic@MacBook-Pro-539.local>
2023-03-07 14:15:25 -0800

    fixing comments

--- navigation.js
+++ navigation.js
@@ -1,8 +1,10 @@
- //navigation bar stuff  
- function makeGray(x){
+
+//Makes the background in navigation bar when hovered over to white
+function makeGray(x){
     x.style.backgroundColor="#999999";
-    }
-    
+}
+
+//Makes the background in navigation bar when hovered out of to black
 function makeBlack(x) {
     x.style.backgroundColor="black";
 }
