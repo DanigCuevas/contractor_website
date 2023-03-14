2f0adb852cc7d57f65b5e581c027aff22b702c6b
DanigCuevas <danielagcuevas@gmail.com>
2023-03-14 22:20:58 +0000

    fixed paths

--- main.py
+++ main.py
@@ -3,14 +3,13 @@
 import os
 import pymysql
 import sqlalchemy
+from sqlalchemy import text
 from flask import Flask, render_template,request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 
 # initializing Flask app
 app = Flask(__name__)
 
-#database
-# db = None
 
 #database google cloud information
 db_user = os.environ.get('CLOUD_SQL_USERNAME')
@@ -18,119 +17,105 @@ db_password = os.environ.get('CLOUD_SQL_PASSWORD')
 db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
 db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
 
-# @app.route('/')
-def getconn():
-    db_config = {
-        # [START cloud_sql_mysql_sqlalchemy_limit]
-        # Pool size is the maximum number of permanent connections to keep.
-        "pool_size": 1,
-        # Temporarily exceeds the set pool_size if no connections are available.
-        "max_overflow": 0,
-        # The total number of concurrent connections for your application will be
-        # a total of pool_size and max_overflow.
-        # [END cloud_sql_mysql_sqlalchemy_limit]
-
-        # [START cloud_sql_mysql_sqlalchemy_backoff]
-        # SQLAlchemy automatically uses delays between failed connection attempts,
-        # but provides no arguments for configuration.
-        # [END cloud_sql_mysql_sqlalchemy_backoff]
-
-        # [START cloud_sql_mysql_sqlalchemy_timeout]
-        # 'pool_timeout' is the maximum number of seconds to wait when retrieving a
-        # new connection from the pool. After the specified amount of time, an
-        # exception will be thrown.
-        "pool_timeout": 30,  # 30 seconds
-        # [END cloud_sql_mysql_sqlalchemy_timeout]
-
-        # [START cloud_sql_mysql_sqlalchemy_lifetime]
-        # 'pool_recycle' is the maximum number of seconds a connection can persist.
-        # Connections that live longer than the specified amount of time will be
-        # reestablished
-        "pool_recycle": 1800,  # 30 minutes
-        # [END cloud_sql_mysql_sqlalchemy_lifetime]
-
-    }
-    return connect_unix_socket(db_config)
-
-def connect_unix_socket(db_config):
-    # """ Initializes a Unix socket connection pool for a Cloud SQL instance of MySQL. """
-    # Note: Saving credentials in environment variables is convenient, but not
-    pool = sqlalchemy.create_engine(
-        # Equivalent URL:
-        # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
-        sqlalchemy.engine.url.URL.create(
-            drivername="mysql+pymysql",
-            username=db_user,
-            password=db_password,
-            database=db_name,
-            query= {"unix_socket": "{}/{}".format(
-                    "/cloudsql", 
-                    db_connection_name) 
-                }
-            # {"unix_socket": unix_socket_path},
-        ),
-    )
-    return pool
-
-
-def view_Customer():
-    cnx = getconn()
-    with cnx.connect() as db_conn:
-        query = "SELECT * FROM CUSTOMER"
-        db_conn.execute(query)
-        customers = db_conn.fetchall()
-        cnx.close()
-        return render_template('home.html',customers=customers)
-
-#Retrieves connection to database => this works 
-# @app.route('/')
-# def get_Conn():
-#     # When deployed to App Engine, the `GAE_ENV` environment variable will be
-#     # set to `standard`
-#     if os.environ.get('GAE_ENV') == 'standard':
-#         # If deployed, use the local socket interface for accessing Cloud SQL
-#         unix_socket = '/cloudsql/{}'.format(db_connection_name)
-#         cnx = pymysql.connect(user=db_user, password=db_password,
-#                               unix_socket=unix_socket, db=db_name)
-#     else:
-#         # If running locally, use the TCP connections instead
-#         # Set up Cloud SQL Proxy (cloud.google.com/sql/docs/mysql/sql-proxy)
-#         # so that your application can use 127.0.0.1:3306 to connect to your
-#         # Cloud SQL instance
-#         host = '127.0.0.1'
-#         cnx = pymysql.connect(user=db_user, password=db_password,
-#                               host=host, db=db_name)                           
-#     with cnx.cursor() as db_conn:
-#         query = "SELECT * FROM CUSTOMER"
-#         db_conn.execute(query)
-#         customers = db_conn.fetchall()
-#         cnx.close()
-#         return render_template('home.html',customers=customers)
-    
+#Retrieves connection to database
+def getConn():
+    # run in google enginge environment
+    if os.environ.get('GAE_ENV') == 'standard':
+        # If deployed, use the local socket interface for accessing Cloud SQL
+        unix_socket = '/cloudsql/{}'.format(db_connection_name)
+        cnx = pymysql.connect(user=db_user, password=db_password,
+                              unix_socket=unix_socket, db=db_name)
+        return cnx
+    else:
+        # If running locally, use the TCP connections instead
+        # Set up Cloud SQL Proxy (cloud.google.com/sql/docs/mysql/sql-proxy)
+        # so that your application can use 127.0.0.1:3306 to connect to your
+        # Cloud SQL instance
+        host = '127.0.0.1'
+        cnx = pymysql.connect(user=db_user, password=db_password,
+                              host=host, db=db_name) 
+        return  cnx                          
     # with cnx.cursor() as db_conn:
-    #     query = "SELECT * FROM JOB"
+    #     query = "SELECT * FROM CUSTOMER"
     #     db_conn.execute(query)
-    #     jobs = db_conn.fetchall()
+    #     customers = db_conn.fetchall()
     #     cnx.close()
-    #     return render_template('job.html',jobs=jobs)
+    #     return render_template('home.html',customers=customers)
+    
+#executes a query from the database 
+def getQuery(cnx,query):
+    with cnx.cursor() as db_conn:   
+        db_conn.execute(query)
+        results = db_conn.fetchall()
+        return results
 
-
-#function to render the home page
+#sends query results to home page
 @app.route('/')
 def home():
-   return render_template('home.html')
+    #retrive database connection
+    cnx = getConn() 
+    #execute query
+    contactorQuery = "SELECT * FROM CONTRACTOR"
+    contractors = getQuery(cnx,contactorQuery)
+    #close connection
+    cnx.close ()
+    return render_template('home.html',contractors=contractors)
 
+#sends query results to login page
 @app.route("/login")
 def login():
-  return render_template("login.html")
+  cnx = getConn()
+  customerQuery = "SELECT * FROM CUSTOMER WHERE CUSTOMER.customer_ID = 603753"
+  customers = getQuery(cnx,customerQuery)
+  cnx.close ()
+  return render_template("login.html",customers=customers)
 
-@app.route("/job")
+#send job query to job page
+@app.route("/job") #THIS NEEDS TO BE FIXED
 def job():
-  return render_template("job.html")
+    cnx = getConn()
+    jobQuery = "SELECT job.service_type, customer.customer_name, contractor.contractor_name, job.job_time, job.job_date\
+                FROM Job, Contractor, Customer \
+                WHERE customer.customer_ID = job.customer_ID \
+                AND contractor.contractor_ID = job.Contractor_ID\
+                AND job.service_type = landscaping AND customer.customer_id = 603753 "
+        #note serv_type should be landscaping  and cust_id should be 603753
+    # jobQuery = jobQuery.bindparams(serv_type="landscaping", cust_id=603753)
+    jobs = getQuery(cnx,jobQuery)
+    cnx.close ()
+    return render_template("job.html",jobs=jobs)
 
+
+#you can create another html file for the sendsEnquiry & use that to show enquiry query 
 @app.route("/contractorReviews")
 def contractorReviews():
+    #need to do this query
+    #SELECT customer_rates_contractor.score, customer_rates_contractor.rating_date, customer_rates_contractor.rating_time, Customer.customer_name, Contractor.contractor_name, customer_rates_contractor.rating_comment
+    #  FROM Customer, Contractor, Job, Customer_rates_contractor 
+    # WHERE customer.customer_ID = job.customer_ID AND job.contractor_ID = contractor.contractor_ID AND job.job_ID = customer_rates_contractor.job_ID and customer.customer_ID = '603753' AND  job.job_ID = '152697';
   return render_template("contractorReviews.html")
+
+# @app.route('/')
+# def main():
+#     cnx = getConn()
+
+#     home(cnx)
+#     login(cnx)
+#     job(cnx)
+#     view = 'home'
+    # if(view == 'home'):
+    # contactorQuery = "SELECT * FROM CONTRACTOR"
+    # contractors = getQuery(cnx,contactorQuery,view)
+    # cnx.close ()
+    # home(contractors)
+        # return render_template('home.html',contractors=contractors)
+    # view = 'Login'
+    # if (view == "login"):
+    # customerQuery = "SELECT * FROM CUSTOMER WHERE CUSTOMER.customer_ID = 603753"
+    # customers = getQuery(cnx,customerQuery,view)
+    # cnx.close ()
+    # return render_template('login.html',customers=customers)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
