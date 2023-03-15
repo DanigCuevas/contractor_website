0695ebe2a65fdb0f824cb4289ca67220615246bc
Daniela Cuevas <danic@MacBook-Pro-539.local>
2023-03-15 01:34:31 -0700

    fixing comments/cleaning up

--- main.py
+++ main.py
@@ -21,26 +21,16 @@ db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
 def getConn():
     # run in google enginge environment
     if os.environ.get('GAE_ENV') == 'standard':
-        # If deployed, use the local socket interface for accessing Cloud SQL
         unix_socket = '/cloudsql/{}'.format(db_connection_name)
         cnx = pymysql.connect(user=db_user, password=db_password,
                               unix_socket=unix_socket, db=db_name)
         return cnx
     else:
-        # If running locally, use the TCP connections instead
-        # Set up Cloud SQL Proxy (cloud.google.com/sql/docs/mysql/sql-proxy)
-        # so that your application can use 127.0.0.1:3306 to connect to your
-        # Cloud SQL instance
+        # to run locally
         host = '127.0.0.1'
         cnx = pymysql.connect(user=db_user, password=db_password,
                               host=host, db=db_name) 
         return  cnx                          
-    # with cnx.cursor() as db_conn:
-    #     query = "SELECT * FROM CUSTOMER"
-    #     db_conn.execute(query)
-    #     customers = db_conn.fetchall()
-    #     cnx.close()
-    #     return render_template('home.html',customers=customers)
     
 #executes a query from the database 
 def getQuery(cnx,query):
@@ -80,13 +70,11 @@ def job():
                 WHERE C.customer_ID = J.customer_ID \
                 AND CON.contractor_ID = J.contractor_ID \
                 AND J.service_type = \'landscaping\' AND C.customer_id = 603753"
-        #note serv_type should be landscaping and cust_id should be 603753
-    # jobQuery = jobQuery.bindparams(serv_type="landscaping", cust_id=603753)
     jobs = getQuery(cnx,jobQuery)
     cnx.close()
     return render_template("job.html",jobs=jobs)
 
-#you can create another html file for the sendsEnquiry & use that to show enquiry query 
+
 @app.route("/contractorReviews/<contractorID>")
 def contractorReviews(contractorID):
     cnx = getConn()
@@ -101,28 +89,6 @@ def contractorReviews(contractorID):
     reviews = getQuery(cnx, reviewsQuery)
     cnx.close()
     return render_template("contractorReviews.html",reviews=reviews)
-
-# @app.route('/')
-# def main():
-#     cnx = getConn()
-
-#     home(cnx)
-#     login(cnx)
-#     job(cnx)
-#     view = 'home'
-    # if(view == 'home'):
-    # contactorQuery = "SELECT * FROM CONTRACTOR"
-    # contractors = getQuery(cnx,contactorQuery,view)
-    # cnx.close ()
-    # home(contractors)
-        # return render_template('home.html',contractors=contractors)
-    # view = 'Login'
-    # if (view == "login"):
-    # customerQuery = "SELECT * FROM CUSTOMER WHERE CUSTOMER.customer_ID = 603753"
-    # customers = getQuery(cnx,customerQuery,view)
-    # cnx.close ()
-    # return render_template('login.html',customers=customers)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
